81f9799a5a7795181d96eec42a889ecbc89410ff
Victor Morales <victor_teo_17@hotmail.com>
2024-01-18 21:12:04 +0100

    agregamos endpoint de perfil de usuario

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,5 @@
 // Importamos las dependencias.
-import express from "express";
+import express from 'express';
 
 // Creamos un router.
 const router = express.Router();
@@ -10,16 +10,20 @@ import {
   loginController,
   sendRecoverPassController,
   editUserPassController,
-} from "../controllers/users/index.js";
+  userProfileController,
+} from '../controllers/users/index.js';
 
+import { authUser } from '../middleware/authUser.js';
 // Crear un usuario
-router.post("/user/register", newUserController);
+router.post('/user/register', newUserController);
 
 // Acceso administrador
-router.post("/user/login", loginController);
+router.post('/user/login', loginController);
+// devuelve el perfil de usuario
+router.get('/user/:userId', authUser, userProfileController);
 
 //  enviar email de recuperacion de contraseña
-router.post("/user/password/recover", sendRecoverPassController);
+router.post('/user/password/recover', sendRecoverPassController);
 // editar contraseña nueva
-router.put("/user/password/reset", editUserPassController);
+router.put('/user/password/reset', editUserPassController);
 export default router;
